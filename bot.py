d2a0bbfdd92fa5e880f48abaf344e90689b1274c
Maria <mariabano.official@gmail.com>
2025-08-27 05:05:16 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import os
 def get_meme():
     response = requests.get('https://meme-api.com/gimme?count=1')
     json_data = json.loads(response.text)
-    return json_data['url'], json_data['title']  
+    return json_data['url'], json_data['title']
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -19,13 +19,15 @@ class MyClient(discord.Client):
 
         if message.content.strip().lower() == '$meme':
             meme_url, meme_title = get_meme()
-
             embed = discord.Embed(title=meme_title, color=discord.Color.random())
             embed.set_image(url=meme_url)
-
             await message.channel.send(embed=embed)
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
-client = MyClient(intents=discord.Intents.all())
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
 client.run(TOKEN)
